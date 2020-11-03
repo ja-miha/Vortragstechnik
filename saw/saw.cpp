cb95153d57c50b90b4448acc50afd1a7fae75563
Jakob Mihatsch <mastermiha@posteo.de>
2020-11-03 21:01:03 +0100

    added random sampling

--- saw/saw.cpp
+++ saw/saw.cpp
@@ -1,100 +1,165 @@
 #include <vector>
 #include <iostream>
 #include <ctime>
-#include <unordered_set>
-#include <boost/functional/hash.hpp>
+#include <set>
+#include <random>
 
-struct vectorhash
+class SelfAvoidingWalk
 {
-    std::size_t operator()(std::vector<int> vec) const noexcept
-    {
-        std::size_t h1 = 3;
-        for (int i = 0; i < vec.size(); i++)
-        {
-            boost::hash_combine(h1, vec[i]);
+
+    private:
+        std::mt19937 gen;
+        std::random_device seed;
+        std::uniform_int_distribution<> dis;
+
+        // return all possible new positions in the range of one step from your old position, that are not included in the history of your walk
+
+        std::vector<std::vector<int> > make_all_steps(std::vector<int> &position, std::set<std::vector<int> > &history){
+            std::vector<std::vector<int> > next_positions;
+            std::vector<int> trial_position;
+            for (int i = 0; i < 3; i++)
+            {
+                trial_position = position;
+                trial_position[i] = trial_position[i]+1;
+                if (history.count(trial_position)==0)
+                {
+                    next_positions.push_back(trial_position);
+                }
+                trial_position = position;
+                trial_position[i] = trial_position[i]-1;
+                if (history.count(trial_position)==0)
+                {
+                    next_positions.push_back(trial_position);
+                }
+            }
+            return next_positions;
         }
-    return h1;
-    }
+
+        // make a random step from your old position and return the new position
+
+        std::vector<int> random_step(std::vector<int> position){
+            int randint = dis(gen);
+            position[(randint-1)/2] = position[(randint-1)/2] + 2*((randint)%2)-1;
+            return position;
+        }
+
+    public:
+
+        // the coordinates reached at each end of a walk are stored here
+
+        std::vector<std::vector<int> > result;
+
+        // initialize the object
+
+        SelfAvoidingWalk(){
+            std::random_device seed;
+            gen = std::mt19937(seed());
+            dis = std::uniform_int_distribution<>(1, 6);
+            std::vector<std::vector<int> > result = std::vector<std::vector<int> >();
+        }
+
+        // find all possible self avoiding walks of length N (only works for small N, maybe up to about 13)
+
+        void walk_everywhere(int N, std::vector<int> position = {0, 0, 0}, std::set<std::vector<int> > history = std::set<std::vector<int> >(), int step = 1){
+        if (step==N)
+        {
+            std::vector<std::vector<int> > next = make_all_steps(position, history);
+            for (int i = 0; i < next.size(); i++)
+            {
+                result.push_back(next[i]);
+            }
+            
+            return;
+        }
+        else if (step==1)
+        {
+            result = std::vector<std::vector<int> >();
+            std::vector<std::vector<int> > next = make_all_steps(position, history);
+            if (step==1)
+            {
+                history.insert(position);
+            }
+            for (int i = 0; i < next.size(); i++)
+            {
+                std::set<std::vector<int> > his = history;
+                his.insert(next[i]);
+                walk_everywhere(N, next[i], his, step+1);
+            }
+        }
+        else
+        {
+            std::vector<std::vector<int> > next = make_all_steps(position, history);
+            if (step==1)
+            {
+                history.insert(position);
+            }
+            for (int i = 0; i < next.size(); i++)
+            {
+                std::set<std::vector<int> > his = history;
+                his.insert(next[i]);
+                walk_everywhere(N, next[i], his, step+1);
+            }
+            
+        }
+        }
+
+        // make a sample of standard random walks until you have encountered sample_size self avoiding random walks of length N 
+
+        void walk_sample(int N, int sample_size){
+            result = std::vector<std::vector<int> >();
+            while (result.size()<sample_size)
+            {
+                std::vector<int> position{0, 0, 0};
+                std::set<std::vector<int> > history{position};
+                //history.insert(position);
+                bool avoidance_flag = false;
+                for (int i = 0; i < N; i++)
+                {
+                    position = random_step(position);
+                    if (history.count(position)==1)
+                    {
+                        avoidance_flag = true;
+                        break;
+                    }
+                    history.insert(position);
+                }
+                if (avoidance_flag==false)
+                {
+                    result.push_back(position);
+                }
+            }
+        }
+
+        // calculate the mean square displacement from result of either walk_sample or walk_everywhere 
+
+        double square_disp(){
+            double square_disp = 0;
+            for (int i = 0; i < result.size(); i++)
+            {
+                for (int j = 0; j < result[i].size(); j++)
+                {
+                    square_disp = square_disp + (result[i][j]*result[i][j]);
+                }
+            }
+            square_disp = square_disp / result.size();
+            return square_disp;
+        }
 };
 
-class saw
-{
-public:
-    std::vector<std::vector<int> > result;
-
-    std::vector<std::vector<int> > make_step(std::vector<int> &position, std::unordered_set<std::vector<int>, vectorhash > &history){
-        std::vector<std::vector<int> > next_positions;
-        std::vector<int> trial_position;
-        for (int i = 0; i < 3; i++)
-        {
-            trial_position = position;
-            trial_position[i] = trial_position[i]+1;
-            if (history.count(trial_position)==0)
-            {
-                next_positions.push_back(trial_position);
-            }
-            trial_position = position;
-            trial_position[i] = trial_position[i]-1;
-            if (history.count(trial_position)==0)
-            {
-                next_positions.push_back(trial_position);
-            }
-        }
-        return next_positions;
-    }
-
-
-    void walk(int N, std::vector<int> position = {0, 0, 0}, std::unordered_set<std::vector<int>, vectorhash > history = std::unordered_set<std::vector<int>, vectorhash >(), int step = 1){
-    if (step==N)
-    {
-        std::vector<std::vector<int> > next = make_step(position, history);
-        for (int i = 0; i < next.size(); i++)
-        {
-            result.push_back(next[i]);
-        }
-        
-        return;
-    }
-    else
-    {
-        std::vector<std::vector<int> > next = make_step(position, history);
-        if (step==1)
-        {
-            history.insert(position);
-        }
-        for (int i = 0; i < next.size(); i++)
-        {
-            std::unordered_set<std::vector<int>, vectorhash > his = history;
-            his.insert(next[i]);
-            walk(N, next[i], his, step+1);
-        }
-        
-    }
-    }
-
-};
-
-
+// how to use: first argument is length of walk N, second is size of your sample.
+//If N is sufficiently small, the excact count is also returned after the sampling result.
 
 int main(int argc, char** argv){
-    std::time_t stime = std::time(nullptr);
+    SelfAvoidingWalk SAW;
     int N = atoi(argv[1]);
-    while (std::time(nullptr)-stime<3600)
+    int sample = atoi(argv[2]);
+    SAW.walk_sample(N, sample);
+    std::cout << SAW.square_disp() << std::endl;
+    if (N<10)
     {
-    saw w;
-    w.walk(N);
-    std::vector<std::vector<int> > res = w.result;
-    long int square_disp = 0;
-    for (int i = 0; i < res.size(); i++)
-    {
-        for (int j = 0; j < res[i].size(); j++)
-        {
-            square_disp = square_disp + (res[i][j]*res[i][j]);
-        }
-        
+        SAW.walk_everywhere(N);
+        std::cout << SAW.square_disp() << std::endl;
     }
-    std::cout << res.size() << " , " << square_disp << std::endl;
-    N = N+1;
-    }
+
     return 1;
 }
--- saw/saw.py
+++ saw/saw.py
@@ -1,26 +1,63 @@
 import numpy as np
-
-ls = []
-
-def make_step(position, history):
-    possible_steps = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
-    possiple_positions = {(position[0]+step[0], position[1]+step[1], position[2]+step[2]) for step in possible_steps}
-    return possiple_positions-history
+import random as rn
 
 
-def saw(N, position=(0, 0, 0), history={(0, 0, 0)}, step=1):
-    global ls
-    if step==N:
-        for element in make_step(position, history):
-            ls.append(element)
-        return
-    else:
-        nex = make_step(position, history)
-        for pos in nex:
-            his=history.copy()
-            his.add(pos)
-            saw(N, position=pos, history=his, step=step+1)
+class SelfAvoidingWalk:
+    def __init__(self):
+        self.result = []
+        self.possible_steps = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
-saw(10)
-#print(ls)
-print(len(ls))
+    def make_all_steps(self, position, history):
+        possiple_positions = {(position[0]+step[0], position[1]+step[1], position[2]+step[2]) for step in self.possible_steps}
+        return possiple_positions-history
+
+    def random_step(self, position):
+        step = rn.choice(self.possible_steps)
+        return (position[0]+step[0], position[1]+step[1], position[2]+step[2])
+
+    def walk_everywhere(self, N, position=(0, 0, 0), history={(0, 0, 0)}, step=1):
+        if step==N:
+            for element in self.make_all_steps(position, history):
+                self.result.append(element)
+            return
+        elif step==1:
+            self.result = []
+            nex = self.make_all_steps(position, history)
+            for pos in nex:
+                his=history.copy()
+                his.add(pos)
+                self.walk_everywhere(N, position=pos, history=his, step=step+1)
+        else:
+            nex = self.make_all_steps(position, history)
+            for pos in nex:
+                his=history.copy()
+                his.add(pos)
+                self.walk_everywhere(N, position=pos, history=his, step=step+1)
+
+    def walk_sample(self, N, sample_size):
+        self.result = []
+        while len(self.result)<sample_size:
+            position=(0, 0, 0)
+            history={(0, 0, 0)}
+            avoidance_flag = False
+            for i in range(N):
+                position = self.random_step(position)
+                if position in history:
+                        avoidance_flag = True
+                        break
+                history.add(position)
+            if not avoidance_flag:
+                self.result.append(position)
+
+    def square_disp(self):
+        s = 0
+        for element in self.result:
+            s += (element[0]**2 + element[1]**2 + element[2]**2)
+        return s/len(self.result)
+
+
+
+
+SAW = SelfAvoidingWalk()
+SAW.walk_sample(5, 1000)
+print(SAW.square_disp())
